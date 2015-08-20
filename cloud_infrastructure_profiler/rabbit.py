b26e52766361a5a58e89252e2ee8505823bbc15b
Sudarshan Acharya <info@sacharya.com>
2015-08-20 16:43:51 +0000

    Adding rabbit agent to track monitor and notifications queue

---
+++ cloud_infrastructure_profiler/rabbit.py
@@ -0,0 +1,33 @@
+import kombu
+
+class Rabbit(object):
+
+    def __init__(self, name):
+        self.conn = kombu.BrokerConnection(hostname="localhost", port=5672, userid='guest', password='guest', virtual_host="/")
+        self.exchange = kombu.Exchange(name, type='topic', durable=False, channel=self.conn.channel())
+        print self.exchange
+        routing_key = name
+        self.queue = kombu.Queue(name=routing_key, routing_key=routing_key, exchange=self.exchange, channel=self.conn.channel(), durable=False)
+        self.queue.declare()
+    
+    def get_messages(self):
+        i=0
+        while True:
+            msg = self.queue.get()
+            if msg is None:
+                break;
+            print "="*25
+            print i
+            i += 1
+            import pprint
+            pprint.pprint(msg.payload)
+            print msg.acknowledged
+            print msg.payload['_unique_id']
+            print msg.payload['message_id']
+            print msg.payload['event_type']
+
+rabbit = Rabbit("monitor.info")
+rabbit.get_messages()
+
+rabbit = Rabbit("notifications.info")
+rabbit.get_messages()

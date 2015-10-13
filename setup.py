941b399424905452cb8e9561e0bed422675693d3
Sudarshan Acharya <info@sacharya.com>
2015-10-13 19:57:34 +0000

    Updatd version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='osprofiler',
-    version='0.1',
+    version='0.2',
     description='OpenStack Profiler Agent',
     packages=find_packages(),
     include_package_data=True,
